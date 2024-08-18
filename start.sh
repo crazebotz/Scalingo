a7c7bb2bb0bdca47a2b5254704b3cd29c11c7b7d
crazebotz <120011436+crazebotz@users.noreply.github.com>
2024-08-18 15:12:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
-git clone $REPO_URL ok && cd ok && pip3 install -U -r requirements.txt
-$START_CMD
+git clone https://github.com/crazebotz/Video-Tool-Bot ok 
+cd ok 
+pip3 install -U -r requirements.txt
+python bot.py
